0cae4b7443575e5fba397e28222369fe5703e71d
Lorenz Hänggi <lolo8304@gmail.com>
2024-04-08 06:56:55 +0200

    fix manual padding

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ class Game {
     this.w = w - 2 * padding;
     this.x = (W - this.w) / 2.0;
     this.y = paddingTop;
+    this.padding = padding;
+    this.paddingTop = paddingTop;
+    this.paddingBottom = paddingBottom;
     this.board = new Board(this.x, this.y, this.w, this.h);
     this.color = Piece.WHITE;
   }
@@ -13,8 +16,9 @@ class Game {
     textSize(40);
     fill("white");
     textAlign(CENTER);
-    const turnText = this.color === Piece.WHITE ? "WHITE's turn" : "BLACK's turn"
-    text(turnText, this.x + this.w/2, this.y-PADDING_TOP+50);
+    const turnText =
+      this.color === Piece.WHITE ? "WHITE's turn" : "BLACK's turn";
+    text(turnText, this.x + this.w / 2, this.y - this.paddingTop + 50);
   }
 
   drawBoad() {
@@ -22,16 +26,19 @@ class Game {
   }
 
   clicked(clientY, clientX) {
-    const selectedIndex = this.board.selectedIndex
+    const selectedIndex = this.board.selectedIndex;
     if (selectedIndex >= 0) {
       const clickedCell = this.board.clickedCell(clientY, clientX);
       if (clickedCell.index != selectedIndex) {
-        this.board.makeMove(selectedIndex, clickedCell.index)
-        this.changeTurn();  
+        this.board.makeMove(selectedIndex, clickedCell.index);
+        this.changeTurn();
       }
-
     } else {
-      const clickedCellForTurn = this.board.clickedCellByColor(clientY, clientX, this.color);
+      const clickedCellForTurn = this.board.clickedCellByColor(
+        clientY,
+        clientX,
+        this.color
+      );
       if (clickedCellForTurn) {
         this.board.clickedToString(clientY, clientX);
         this.board.selectCellIndex(clickedCellForTurn.index);
