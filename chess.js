ee2228bf30448d5d57e0d212faf14ee2c3c1c7b1
Lorenz Hänggi <lolo8304@gmail.com>
2024-05-05 10:35:33 +0200

    hide test for not localhost

--- chess.js
+++ chess.js
@@ -103,7 +103,8 @@ function undoLastMove() {
 }
 
 function testMoves() {
-  const maxDepth = 3;
+  console.log("Start test move calculation:");
+  maxDepth = 2;
   for (let depth = 1; depth < maxDepth + 1; depth++) {
     const startTime = performance.now();
     const numPositions = game.board.data.testMoves(depth);
